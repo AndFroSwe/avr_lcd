e94795f04844601cecbcdbe78e80e0af1d618ca9
andfroswe <gummianka88@gmail.com>
2017-10-08 15:51:13 +0200

    Added functions for writing to lcd

--- src/avr_lcd.c
+++ src/avr_lcd.c
@@ -19,3 +19,64 @@ int main(int argc, char *argv[])
 
     return 0;
 }
+
+int send_cmd(int cmd)
+{
+    // Send first MS bits, then LS
+    int nibble;
+    for (int i = 1; i => 0; --i) {
+        nibble = cmd >> 4*i;
+        // Enable pins
+        if (nibble & 0x01) {
+            PIN_ON(DB4_PORT, DB4_PIN);
+        } else {
+            PIN_OFF(DB4_PORT, DB4_PIN);
+        }
+        if ((nibble >> 1) & 0x01) {
+            PIN_ON(DB5_PORT, DB5_PIN);
+        } else {
+            PIN_OFF(DB5_PORT, DB5_PIN);
+        }
+        if ((nibble >> 2) & 0x01) {
+            PIN_ON(DB6_PORT, DB6_PIN);
+        } else {
+            PIN_OFF(DB6_PORT, DB6_PIN);
+        }
+        if ((nibble >> 3) & 0x01) {
+            PIN_ON(DB7_PORT, DB7_PIN);
+        } else {
+            PIN_OFF(DB7_PORT, DB7_PIN);
+        }
+
+        // Send message
+        PIN_ON(EN_PORT, EN_PIN);
+        _delay_ms(100);
+        PIN_OFF(EN_PORT, EN_PIN);
+    }
+
+    return 0;
+}
+
+int print_char(char c)
+{
+    // Set to write to DR
+    PIN_ON(RS_PORT, RS_PIN);
+    PIN_OFF(RW_PORT, RW_PIN);
+    
+    // Send command
+    send_cmd(c);
+
+    return 0;
+}
+
+int send_instr(int i)
+{
+    // Set to write to DR
+    PIN_OFF(RS_PORT, RS_PIN);
+    PIN_OFF(RW_PORT, RW_PIN);
+
+    // Send command
+    send_cmd(i);
+
+    return 0;
+}
--- src/avr_lcd.h
+++ src/avr_lcd.h
@@ -17,9 +17,22 @@
 #define RS_PIN PB0
 #define EN_PORT PORTB
 #define EN_PIN PB1
+#define RW_PORT PORTB
+#define RW_PIN PB2
+#define DB4_PORT PORTB
+#define DB4_PIN PB3
+#define DB5_PORT PORTB
+#define DB5_PIN PB4
+#define DB6_PORT PORTB
+#define DB6_PIN PB5
+#define DB7_PORT PORTB
+#define DB7_PIN PB6
 
 // Includes
 #include <avr/io.h>
 #include <util/delay.h>
 
-
+// Function prototypes
+int send_cmd(int cmd);
+int print_char(int c);
+int send_instr(int i);
